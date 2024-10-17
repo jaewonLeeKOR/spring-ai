c25b2837355ee8e97c3a1ff04a5e598506c0351e
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-10-18 01:14:56 +0900

    인메모리 챗 메모리 구현 및 페르소나 부여를 통한 프롬프팅

--- src/main/java/com/example/demo/ai/AiController.java
+++ src/main/java/com/example/demo/ai/AiController.java
@@ -1,8 +1,14 @@
 package com.example.demo.ai;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.huggingface.HuggingfaceChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +21,45 @@ import java.time.Duration;
 public class AiController {
     @Autowired
     private OpenAiChatModel openAiChatModel;
+    private final ChatMemory chatMemory = new InMemoryChatMemory();
 
-    @PostMapping(value = "/openai/chat")
-    public Flux<String> openAi(@RequestBody String chatMessage) {
+    @PostMapping(value = "/openai/chat/interviewer/cto")
+    public Flux<String> openAiInterviewerCto(@RequestBody String chatMessage) {
         ChatClient chatClient = ChatClient.builder(openAiChatModel).build();
-        String responseContent = chatClient.prompt().user(chatMessage).call().content();
+        String responseContent = chatClient
+            .prompt(new Prompt("", OpenAiChatOptions.builder().withModel(OpenAiApi.ChatModel.GPT_4_O).build()))
+            .advisors(new MessageChatMemoryAdvisor(chatMemory, "InterviewerCto", 100))
+            .system(PromptMessage.InterviewerCto.message)
+            .user(chatMessage)
+            .call().content();
+        return Flux.just(responseContent);
+    }
+
+    //TODO 특정 식당에 대한 메뉴판 데이터베이스를 저장, RAG 통해 메뉴 추천 받도록 구현 계획
+    @PostMapping(value = "/openai/chat/diet-manager")
+    public Flux<String> openAiDietManager(@RequestBody String chatMessage) {
+        ChatClient chatClient = ChatClient.builder(openAiChatModel).build();
+        String responseContent = chatClient
+            .prompt(new Prompt("", OpenAiChatOptions.builder().withModel(OpenAiApi.ChatModel.GPT_4_O).build()))
+            .advisors(new MessageChatMemoryAdvisor(chatMemory, "diet-manager", 100))
+            .system(PromptMessage.DietManager.message)
+            .user(chatMessage)
+            .call().content();
+        return Flux.just(responseContent);
+    }
+
+    //TODO openAI 이용한 구현 완료 후 huggingface에서 모델 선정과 추가 구현 계획
+    @PostMapping(value = "/huggingface/chat")
+    public Flux<String> huggingFace(@RequestBody String chatMessage) {
+        String apiToken = "", basePath = "";
+        HuggingfaceChatModel huggingfaceChatModel = new HuggingfaceChatModel(apiToken, basePath);
+        ChatClient chatClient = ChatClient.builder(huggingfaceChatModel).build();
+        String responseContent = chatClient
+            .prompt()
+            .advisors(new MessageChatMemoryAdvisor(chatMemory, "InterviewerCto-huggingface", 100))
+            .system("대한민국의 5살인 떡볶이를 좋아하는 미취학 아동이야 기본적인 지식이 많이 없고 노는걸 제일 좋아해. 기본적인 교육을 받지 못했고 궁금한게 많은 아이야")
+            .user(chatMessage)
+            .call().content();
         return Flux.just(responseContent);
     }
 
---
+++ src/main/java/com/example/demo/ai/PromptMessage.java
@@ -0,0 +1,18 @@
+package com.example.demo.ai;
+
+public enum PromptMessage {
+    InterviewerCto("""
+        당신은 MIT에서 컴퓨터과학 전공으로 학사에서 박사까지 취득했고, 여러 웹 오픈소스 프레임워크 개발에도 참여하고 있는 개발자야.
+        지금은 IT기업의 CTO 자리에서 신입사원의 면접관으로 면접에 참여하고 있는 상황이야. 질문은 한번에 한종류만 가능하고 꼬리 질문은 한 종류에 대해서는 3번까지만 가능해.
+        면접관의 관점에서 봤을때 면접자의 답변에서 고쳐야할점을 괄호 안에 넣어서 답해주는데 대화에서는 이것을 얘기하지 않은것으로 간주해줘.
+        """),
+    DietManager("""
+        당신은 영양관리와 의학에서의 전문적 지식을 가진 영양사이자, 식단 관리자야.
+        식단 관리에 대한 질문을 받아 답변해주는 역할을 하고 있어.
+        """),
+    ;
+    public final String message;
+    PromptMessage(String message) {
+        this.message = message;
+    }
+}
